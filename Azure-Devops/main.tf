8d4c291e10bd983134e0743c624c80868d5502b7
mysticrenji <mysticrenji@gmail.com>
2020-09-10 08:08:31 +0000

    Added Azure Devops Sample Project

---
+++ Azure-Devops/main.tf
@@ -0,0 +1,29 @@
+provider "azuredevops" {
+  version = ">= 0.0.1"
+}
+
+resource "azuredevops_project" "project" {
+  project_name = "Terraforming-AzDevOps"
+  description  = "Terraformed using GitHub Actions"
+}
+
+resource "azuredevops_git_repository" "repository" {
+  project_id = azuredevops_project.project.id
+  name       = "Terraformed"
+  initialization {
+    init_type = "Clean"
+  }
+}
+
+resource "azuredevops_build_definition" "build_definition" {
+  project_id = azuredevops_project.project.id
+  name       = "My Awesome Build Pipeline"
+  path       = "\\"
+
+  repository {
+    repo_type   = "TfsGit"
+    repo_id     = azuredevops_git_repository.repository.id
+    branch_name = azuredevops_git_repository.repository.default_branch
+    yml_path    = "azure-pipelines.yml"
+  }
+}
