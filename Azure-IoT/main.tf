30757efe782fbd0b9cbce6c0231974ee4a7dcf79
mysticrenji <mysticrenji@gmail.com>
2020-09-10 09:11:36 +0000

    Azure-IoT

---
+++ Azure-IoT/main.tf
@@ -0,0 +1,31 @@
+## <https://www.terraform.io/docs/providers/azurerm/index.html>
+provider "azurerm" {
+    version = "~>2.0"
+    features {}
+}
+
+terraform {
+ backend "remote" {
+   organization = "mysticrenji"
+
+   workspaces {
+     name = "terraform-azureiot"
+   }
+ }
+}
+
+
+resource "azurerm_resource_group" "azureiot" {
+  name     = "terraform-azureiot"
+  location = "us west"
+}
+
+resource "azurerm_iothub" "azureiothub" {
+  name                = "Example-IoTHub"
+  resource_group_name = azurerm_resource_group.azureiot.name
+  location            = azurerm_resource_group.azureiot.location
+
+  sku {
+    name     = "F1"
+    capacity = "1"
+  }
