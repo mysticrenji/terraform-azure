e20adca3f775dd2c25f907d47e42467e05494bc4
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2020-09-11 10:48:30 +0800

    Update main.tf

--- Azure-IoT/main.tf
+++ Azure-IoT/main.tf
@@ -1,6 +1,8 @@
-provider "azurerm" {
-    version = "~>2.0"
-    features {}
+rprovider "azurerm" {
+  # AzureRM provider 2.x
+  version = "~>2.0"
+  # v2.x required "features" block
+  features {}
 }
 
 terraform {
@@ -13,18 +15,44 @@ terraform {
  }
 }
 
-
-resource "azurerm_resource_group" "azureiot" {
-  name     = "terraform-azureiot"
-  location = "us west"
+# Create a resource group
+resource "azurerm_resource_group" "b59" {
+  name     = "b59-iot-rg"
+  location = "North Central US"
 }
 
-resource "azurerm_iothub" "azureiothub" {
-  name                = "Example-IoTHub"
-  resource_group_name = azurerm_resource_group.azureiot.name
-  location            = azurerm_resource_group.azureiot.location
+# Create an Azure IoT Hub
+resource "azurerm_iothub" "b59iothub" {
+    name                = "b59iothub"
+    resource_group_name = azurerm_resource_group.b59.name
+    location            = azurerm_resource_group.b59.location
 
-  sku {
-    name     = "F1"
-    capacity = "1"
-  }
+    sku {
+        name     = "F1"
+        capacity = 1
+    }
+}
+
+# Create an IoT Hub Access Policy
+data "azurerm_iothub_shared_access_policy" "b59iothub_iothubowner" {
+    name                = "iothubowner"
+    resource_group_name = azurerm_resource_group.b59.name
+    iothub_name         = azurerm_iothub.b59iothub.name
+}
+
+# Create a Device Provisioning Service
+resource "azurerm_iothub_dps" "b59dps" {
+    name                = "b59dps"
+    resource_group_name = azurerm_resource_group.b59.name
+    location            = azurerm_resource_group.b59.location
+
+    sku {
+        name     = "F1"
+        capacity = 1
+    }
+
+    linked_hub {
+        connection_string = data.azurerm_iothub_shared_access_policy.b59iothub_iothubowner.primary_connection_string
+        location = azurerm_iothub.b59iothub.location
+    }
+}
